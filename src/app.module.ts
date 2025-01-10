f8c13111a393488ccf620134bb3b1258034d86fd
limitlessfavour <oketolulope3@gmail.com>
2025-01-10 08:25:21 +0100

    chore: deploy remote db instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,14 +31,17 @@ import { WebhookModule } from './webhooks/webhook.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'smartinvoice',
+      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [
         Invoice,
         InvoiceItem,
@@ -53,7 +56,11 @@ import { WebhookModule } from './webhooks/webhook.module';
         BulkClientUploadJob,
         BulkClientUploadItem,
       ],
-      synchronize: true,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
+      synchronize: process.env.NODE_ENV !== 'production',
       autoLoadEntities: true,
     }),
     ClientModule,
